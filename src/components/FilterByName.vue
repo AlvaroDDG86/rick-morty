474a1ddbff682954575937bb78e552b8910bc142
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-01-10 21:15:14 +0100

    New behavour in filterByName

--- src/components/FilterByName.vue
+++ src/components/FilterByName.vue
@@ -4,8 +4,11 @@
       type="text"
       placeholder="Search by name"
       :value="modelValue"
-      @input="(event) => update(event)" 
+      @keyup.enter="(event) => update(event)"
     />
+    <span v-show="!!modelValue" class="search__clear" @click="_ => clear()">
+      X
+    </span>
   </div>
 </template>
 <script>
@@ -16,29 +19,43 @@ export default {
         require: true
       }
     },
-    setup(_, { emit }) {
+    setup(props, { emit }) {
         function update(event) {
           emit('update:modelValue', event.target.value)
         }
+        function clear(event) {
+          emit('update:modelValue', '')
+        }
         return {
-            update
+            update,
+            clear
         }
     }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .search {
-  width: 100%;
+  width: 50%;
+  margin-left: 25%;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 1em 0;
+  position: relative;
   input {
+    width: 100%;
     height: 3em;
-    width: 50%;
     border: none;
     border-radius: 10px;
     padding: 0 0.5rem;
+  }
+  &__clear {
+    position: absolute;
+    font-weight: bold;
+    margin: auto;
+    right: 1em;
+    color: black;
+    cursor: pointer;
   }
 }
 </style>

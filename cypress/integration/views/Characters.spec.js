07de85a30f15522241a87ad3a5364c95cb08c8b7
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-01-20 18:13:51 +0100

    New conf for cypress testing

---
+++ cypress/integration/views/Characters.spec.js
@@ -0,0 +1,6 @@
+describe('Characters tests', () => {
+    it('shoul loads de view', () => {
+        cy.visit('/')
+        // cy.get('[data-cy="characters-list"]').should('be.visible')
+    })
+})
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,10 +2,8 @@ const { startDevServer } = require('@cypress/webpack-dev-server')
 const webpackConfig = require('@vue/cli-service/webpack.config')
 
 module.exports = (on, config) => {
-  on('dev-server:start', (options) => {
-    return startDevServer({
-      options,
-      webpackConfig,
-    })
+  return Object.assign({}, config, {
+    fixturesFolder: 'cypress/fixtures',
+    supportFile: 'cypress/support/index.js'
   })
 }
